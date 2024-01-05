f791229d10152662246ca035f67b8253ed7b6d94
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2024-01-05 09:27:56 +0000

    finds

---
+++ arrayMethods/find.js
@@ -0,0 +1,11 @@
+// the find method return the first element that satisfies the test
+// The find() method of Array instances returns the first element in the
+// provided array that satisfies the provided testing function.
+// If no values satisfy the testing function, undefined is returned.
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find((element) => element > 10);
+
+console.log(found);
+// Expected output: 12
