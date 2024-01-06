ace603b7cd3914a9e546114d9538c682ccaf02dd
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2024-01-06 20:42:48 +0000

    added array reverse

---
+++ arrayMethods/reverse.js
@@ -0,0 +1,13 @@
+//  other words, elements order in the array will be
+//  turned towards the direction opposite to that previously stated.
+const array1 = ["one", "two", "three"];
+console.log("array1:", array1);
+// Expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = array1.reverse();
+console.log("reversed:", reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log("array1:", array1);
+// Expected output: "array1:" Array ["three", "two", "one"]
